44bcbd831d77231c5f3a98b3278d85358e406d98
Gaurav Jadhav <gauravjadhav173@gmail.com>
2024-07-22 18:59:46 +0530

    db

--- backend/socket.js
+++ backend/socket.js
@@ -80,10 +80,10 @@ async function handleGetTabData(req, res) {
 // Function to fetch live open tabs data
 async function fetchLiveTabs(openTabs) {
   const urls = Object.values(openTabs);
-  const db = 'SELECT url FROM "data".aiurl WHERE url = ANY($1::text[])';
+  const query = 'SELECT url FROM "data".aiurl WHERE url = ANY($1::text[])';
 
   try {
-    const result = await db.query(db, [urls]);
+    const result = await db.query(query, [urls]);
     const existingUrls = result.rows.map(row => row.url);
     const filteredTabs = {};
 
